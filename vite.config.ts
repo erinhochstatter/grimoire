9ac5123a1da293dab9f9edcb16ce6c92f79adf7f
Erin Hochstatter <ehochstatter@gmail.com>
2024-05-20 21:24:32 -0500

    add testing libraries & fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      typekit:
+      {
+        id: 'vxj5fru',
+        defer: true,
+        injectTo: 'head-prepend',
+      },
+    })
+  ],
 })
