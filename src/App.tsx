7c00a2522aa0c26bf691183c6fdbf005e15229f2
Erin Hochstatter <ehochstatter@gmail.com>
2024-07-01 08:05:59 -0500

    Add react-query for async state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState, version } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
 import { HomePage } from './Pages/Home/HomePage'
 import { NavigationHeader } from './Layout/NavigationHeader/NavigationHeader'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div>
-      <NavigationHeader />
-      <HomePage initialTab='books' />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <NavigationHeader />
+        <HomePage initialTab='books' />
+      </div>
+    </QueryClientProvider>
   )
 }
 
