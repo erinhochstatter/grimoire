0cbf8fd2f64a403ef5a63be079fb040644ef1540
Erin Hochstatter <ehochstatter@gmail.com>
2024-07-04 12:29:06 -0500

    Add Test Setup

---
+++ src/Pages/Home/Rows/PostRow.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { format } from 'date-fns'
+import styles from './PostRow.module.css'
+import { ModalIcon } from '../../../assets/ModalIcon'
+
+interface Props {
+  post: Post
+}
+
+export const PostRow: React.FC<Props> = ({ post }: Props) => {
+  console.log()
+  // TODO: make the dot a square
+  const dot = '\u2022'
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <div className={styles.titleText}>
+          <p className={styles.text}>{format(post.datePublished, 'MM.dd.yy')}</p>
+          <div className={styles.titleWithButton}>
+            <p className={styles.text}>
+              {post.author}: {post.title}
+            </p>
+            <button className={styles.button}>
+              {/* TODO: fix the icon on hover */}
+              <ModalIcon fill='#46795a' />
+            </button>
+          </div>
+        </div>
+        <div className={styles.details}>
+          <p className={styles.text}>
+            {dot} {post.kind}
+          </p>
+          <p className={styles.text}>Comments</p>
+        </div>
+      </div>
+      <img src={post.coverImagePath ?? 'https://picsum.photos/200/300'} className={styles.image} />
+    </div>
+  )
+}
